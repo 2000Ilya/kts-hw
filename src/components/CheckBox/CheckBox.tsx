3ae787cee04e8217ec2d020cb83e35947f96b6b7
Ilya Konik <konick.ilya@yandex.ru>
2022-08-31 01:10:44 +0300

    CheckBox component added

---
+++ src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,20 @@
+type CheckBoxProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "onChange"
+> & {
+  onChange: (value: boolean) => void;
+};
+
+const CheckBox = ({ onChange, ...restProps }: CheckBoxProps) => {
+  return (
+    <input
+      {...restProps}
+      className={"checkbox"}
+      checked={restProps.checked}
+      onChange={(event) => onChange(event.target.checked)}
+      type={"checkbox"}
+    />
+  );
+};
+
+export default CheckBox;
