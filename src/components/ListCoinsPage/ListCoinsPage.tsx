9f442234fc162fa6aba082a82b249e3eb02ec1ce
Ilya Konik <konick.ilya@yandex.ru>
2022-09-07 17:33:09 +0300

    List component and search added

--- src/components/ListCoinsPage/ListCoinsPage.tsx
+++ src/components/ListCoinsPage/ListCoinsPage.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 import { Button } from "@components/Button";
-import Card from "@components/Card";
 import Input from "@components/Input";
-import { Loader, LoaderSize } from "@components/Loader";
+import List from "@components/List/List";
+import { Loader } from "@components/Loader";
 import CoinsListStore from "@store/CoinGeckoStore/CoinsListStore";
 import { Meta } from "@utils/meta";
-import roundNumber from "@utils/roundNumber";
 import { useLocalStore } from "@utils/useLocalStore";
 import classNames from "classnames";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { Link } from "react-router-dom";
+import { createSearchParams, useSearchParams } from "react-router-dom";
 
 import styles from "./ListCoinsPage.module.scss";
 
 const ListCoinsPage: React.FC = React.memo(() => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [inputValue, setInputValue] = useState<string>("");
   const coinsListStore = useLocalStore(() => new CoinsListStore());
+
+  const handleSearchInputChange = (text: string): void => {
+    setSearchParams(createSearchParams({ search: text }));
+    setInputValue(text);
+  };
 
   useEffect(() => {
     coinsListStore.getCoinsList({
@@ -35,56 +39,24 @@ const ListCoinsPage: React.FC = React.memo(() => {
         <Input
           placeholder={"Search Cryptocurrency"}
           value={inputValue}
-          onChange={setInputValue}
+          onChange={handleSearchInputChange}
         />
         <Button>Search</Button>
       </div>
       {coinsListStore.meta === Meta.sucsess ? (
-        <div
-          className={classNames(styles["coins-list-container"])}
-          id={"coins-list"}
-        >
-          <InfiniteScroll
-            dataLength={coinsListStore.list.length}
-            next={() =>
-              coinsListStore.fetchMoreCoins({
-                queryParameters: {
-                  vs_currency: "usd",
-                  order: "market_cap_desc",
-                  per_page: 10,
-                  sparkline: false,
-                },
-              })
-            }
-            hasMore={true}
-            loader={
-              <div
-                className={classNames(
-                  styles["flex-align-center"],
-                  styles["list-coins-page__loader-container"]
-                )}
-              >
-                <Loader size={LoaderSize.s} />
-              </div>
-            }
-            scrollableTarget={"coins-list"}
-          >
-            {coinsListStore.list.map((coinItem) => (
-              <Link to={`/coins/${coinItem.id}`}>
-                <Card
-                  key={coinItem.id}
-                  image={coinItem.image}
-                  title={coinItem.name}
-                  subtitle={coinItem.symbol}
-                  currentPrice={`${roundNumber(coinItem.currentPrice)}`}
-                  priceChangePercentage={`${roundNumber(
-                    coinItem.priceChangePercentage24h
-                  )}%`}
-                />
-              </Link>
-            ))}
-          </InfiniteScroll>
-        </div>
+        <List
+          loadMore={() =>
+            coinsListStore.fetchMoreCoins({
+              queryParameters: {
+                vs_currency: "usd",
+                order: "market_cap_desc",
+                per_page: 10,
+                sparkline: false,
+              },
+            })
+          }
+          list={coinsListStore.list}
+        />
       ) : (
         <div
           className={classNames(
